ef0331b752e7675cba8d6f97e221ccd2c11841e8
Drew Gray <drewjgray@gmail.com>
2024-11-25 08:12:47 -1000

    added title

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <h1 class="title">Expedition Labs</h1>
   <Earth />
 </template>
 
@@ -10,6 +11,7 @@ import Earth from './components/Earth.vue'
 body {
   margin: 0;
   padding: 0;
+  background-color: black;
 }
 
 #app {
@@ -18,5 +20,16 @@ body {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: black;
+  background-color: black;
+}
+
+.title {
+  color: white;
+  font-size: 3rem;
+  margin: 1rem 0;
+  position: relative;
+  z-index: 1;
+  margin-top: 40px;
+  margin-bottom: -140px;
 }
 </style>
